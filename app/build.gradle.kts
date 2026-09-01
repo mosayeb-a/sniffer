bf86d5866ec1d24a0cc06208028245b5b44b8404
mosayeb <angashtemosayeb@gmail.com>
2026-09-01 13:58:36 +0330

    feat: add fastlane metadata and bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.ma.sniffer"
         minSdk = 23
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
